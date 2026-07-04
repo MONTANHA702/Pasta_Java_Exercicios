50abff1f828d2bbff0a972bfefde50b8007e8aba
Jader <jaderssantos@gmail.com>
2026-07-04 20:11:14 -0300

    Mais exercicios

---
+++ src/exercicios/Ex3MediaQuatroValoresArray.java
@@ -0,0 +1,30 @@
+package exercicios;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex3MediaQuatroValoresArray {
+    public static void main(String[] args) {
+        //Usando Array
+
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+        double[] notas = new double[4];
+
+        for (int i = 0; i < notas.length; i++ ){
+            System.out.printf("\nDigite a %da nota: ", i+1);
+            notas[i] = sc.nextDouble();
+        }
+        double media = Arrays.stream(notas).average().orElse(0);
+        String linha = "-".repeat(15);
+
+        System.out.println();
+        System.out.println(linha);
+        System.out.println("Notas: "+ Arrays.toString(notas));
+        System.out.printf("Media: %.3f\n", media);
+        System.out.println(linha);
+
+    }
+}

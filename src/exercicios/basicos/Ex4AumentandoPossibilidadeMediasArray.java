3a85b3d10049b39f11bec90240a34f58e0d6b12d
Jader <jaderssantos@gmail.com>
2026-07-05 21:36:06 -0300

    Exercios 10 com TimeUnit para treinar

--- src/exercicios/basicos/Ex4AumentandoPossibilidadeMediasArray.java
+++ src/exercicios/basicos/Ex4AumentandoPossibilidadeMediasArray.java
@@ -1,4 +1,4 @@
-package exercicios;
+package exercicios.basicos;
 
 import java.util.Arrays;
 import java.util.Locale;
---
+++ src/exercicios/basicos/Ex9Tabuada.java
@@ -0,0 +1,22 @@
+package exercicios.basicos;
+
+
+public class Ex9Tabuada {
+    public static void main(String[] args) {
+        //Tabuada de 7
+
+        int n1 = 7;
+        String linha = "=".repeat(10);
+
+        System.out.println(linha);
+        System.out.println("TABUADA DE 7");
+
+        System.out.println(linha);
+        for (int i =1; i <= 10; i++){
+            int tabuada = n1 * i;
+            System.out.printf("%d X %d = %d%n", n1, i, tabuada);
+
+        }
+        System.out.println(linha);
+    }
+}

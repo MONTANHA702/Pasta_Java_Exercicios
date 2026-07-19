10af0283323a6bf2f4fac94480c5a412a5401d9f
Jader <jaderssantos@gmail.com>
2026-07-19 20:25:26 -0300

    Relacionamento classes tem um

---
+++ src/exercicios/poo/ex16relacionamentoclasses/Endereco.java
@@ -0,0 +1,32 @@
+package exercicios.poo.ex16relacionamentoclasses;
+
+public class Endereco {
+
+    private String cidade;
+    private String cep;
+    private String estado;
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}

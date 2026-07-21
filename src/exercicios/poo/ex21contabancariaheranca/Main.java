67295bc02788ed13aaa5b2b16f0767f1ad9148a9
Jader <jaderssantos@gmail.com>
2026-07-21 20:28:40 -0300

    Exercício de herança com biblioteca Date

---
+++ src/exercicios/poo/ex21contabancariaheranca/Main.java
@@ -0,0 +1,16 @@
+package exercicios.poo.ex21contabancariaheranca;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ContaPoupanca cp = new ContaPoupanca("Marta", "123435", 12000);
+        System.out.println(cp.toString());
+
+
+
+
+
+
+
+    }
+}

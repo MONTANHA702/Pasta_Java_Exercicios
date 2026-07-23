be33797700ba5f79c1c8c6eef14c4c5ad4e1e818
Jader <jaderssantos@gmail.com>
2026-07-23 12:14:51 -0300

    Exceções múltiplas

---
+++ src/exercicios/basicos/Ex13ExcecoesMultiplas.java
@@ -0,0 +1,20 @@
+package exercicios.basicos;
+
+public class Ex13ExcecoesMultiplas {
+    public static void main(String[] args) {
+
+        int[] numeradores = {2, 4, 6, 8};
+        int[] denominadores = {2, 0, 3};
+
+        for (int i = 0; i < numeradores.length; i++) {
+            try {
+                System.out.printf("%d / %d = %d%n", numeradores[i], denominadores[i], numeradores[i] / denominadores[i]);
+            } catch (ArithmeticException e) {
+                System.out.println("Divisão por zero.");
+            } catch(ArrayIndexOutOfBoundsException e){
+                System.out.println("Arrays de tamanhos diferentes.");
+            }
+        }
+
+    }
+}

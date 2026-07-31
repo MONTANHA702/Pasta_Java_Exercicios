bc6d44f091c2328f8736b2ed771cdf6192d49248
Jader <jaderssantos@gmail.com>
2026-07-31 16:20:48 -0300

    Classes aninhadas: classe interna com comentarios

--- STRINGS/src/Externa.java
+++ STRINGS/src/Externa.java
@@ -2,17 +2,20 @@ public class Externa {
 
     private String texto = "Texto Externo";
 
-    public class Interna {
+    public  static class Interna {
         private String texto = "Texto Interno";
         public void verTexto() {
             System.out.println(texto);
-            System.out.println(Externa.this.texto);
+            System.out.println(this.texto); //se nao fosse estatic seria Externa.this.texto
         }
     }
 
     public static void main(String[] args) {
          Externa ex = new Externa();
-         Interna interna = ex.new Interna();
+
+         //se a classe Interna nao fosse estatic seria a sintaxe
+        //Interna interna = ex.new Interna()
+         Interna interna = new Interna();
 
          interna.verTexto();
     }

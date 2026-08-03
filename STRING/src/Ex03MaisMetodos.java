14d3171c59bc362197d0ea0b30dcfe3a44f84eee
Jader <jaderssantos@gmail.com>
2026-08-03 19:15:25 -0300

    métodos de comparação

---
+++ STRING/src/Ex03MaisMetodos.java
@@ -0,0 +1,24 @@
+public class Ex03MaisMetodos {
+    public static void main(String[] args) {
+
+        String ola = "Olá";
+        String ola2 = "OLÁ";
+        String ola3 = "Olá";
+
+        //equals() é case sensitive
+        System.out.println(ola.equals(ola2));//false
+        System.out.println(ola.equals(ola3));//true
+
+        //equalsIgnoreCase() ignora se é maiúsculo ou minúsculo
+        System.out.println(ola.equalsIgnoreCase(ola2));//true
+
+        //.endsWith() e .startsWith()
+        String banana = "banana";
+        System.out.println(banana.endsWith("ana"));
+        System.out.println(banana.startsWith("ban"));
+
+        //.compareTo(): tendo a e b, retorna -1 se a>b, 0 se a==b e 1 se a<b usado em Arraylist
+
+
+    }
+}

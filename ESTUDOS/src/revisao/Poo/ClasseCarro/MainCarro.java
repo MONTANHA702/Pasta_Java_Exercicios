5472a3d9f8a26367ea12fcac0903e2215609878e
MONTANHA702 <jaderssantos@gmail.com>
2026-08-18 17:15:51 -0300

    instanciando classe - ferramenta Java Visualizer

---
+++ ESTUDOS/src/revisao/Poo/ClasseCarro/MainCarro.java
@@ -0,0 +1,10 @@
+package revisao.Poo.ClasseCarro;
+
+public class MainCarro {
+    public static void main(String[] args) {
+        //instanciar uma classe
+        Carro meuCarro = new Carro();
+        Carro seuCarro = new Carro();
+        Carro outroCarro = new Carro();
+    }
+}

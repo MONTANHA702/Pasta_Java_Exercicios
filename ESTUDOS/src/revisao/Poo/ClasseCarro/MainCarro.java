223a80c51312472f20360a7e736744fafc777967
MONTANHA702 <jaderssantos@gmail.com>
2026-08-18 17:42:10 -0300

    inserindo atributos - Java Visualizer

--- ESTUDOS/src/revisao/Poo/ClasseCarro/MainCarro.java
+++ ESTUDOS/src/revisao/Poo/ClasseCarro/MainCarro.java
@@ -1,10 +1,27 @@
 package revisao.Poo.ClasseCarro;
 
+
+
 public class MainCarro {
     public static void main(String[] args) {
         //instanciar uma classe
         Carro meuCarro = new Carro();
         Carro seuCarro = new Carro();
-        Carro outroCarro = new Carro();
+
+
+        //inserindo atributos
+        meuCarro.modelo = "X5";
+        meuCarro.cor = "azul";
+        meuCarro.anoFabricacao = 2026;
+        meuCarro.fabricante = "BMW";
+
+        seuCarro.modelo = "AIRCROSS";
+        seuCarro.cor = "branco";
+        seuCarro.anoFabricacao = 2016;
+        seuCarro.fabricante = "CRITROEN";
+
+        System.out.println("Meu carro é um " + meuCarro.modelo);
+        System.out.println("Seu carro é um " + seuCarro.modelo);
+
     }
 }

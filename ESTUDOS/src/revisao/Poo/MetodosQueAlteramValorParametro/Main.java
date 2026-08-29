1458e4c107612731c47abebc1352ba88ebf702b7
MONTANHA702 <jaderssantos@gmail.com>
2026-08-29 15:56:30 -0300

    metodos que alteram o valor de parametro do tipo primitivo

---
+++ ESTUDOS/src/revisao/Poo/MetodosQueAlteramValorParametro/Main.java
@@ -0,0 +1,16 @@
+package revisao.Poo.MetodosQueAlteramValorParametro;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ServicoDePrecificacao novoServico = new ServicoDePrecificacao();
+
+        double valorCompraFornecedor = 140;
+        novoServico.calcularPrecoVenda(valorCompraFornecedor);//altera e morre no proprio metodo
+        System.out.println(valorCompraFornecedor);//140
+
+        //forma correta
+//        double novoPreco = novoServico.calcularPrecoVenda(valorCompraFornecedor);
+//        System.out.println(novoPreco);
+    }
+}
---
+++ ESTUDOS/src/revisao/Poo/MetodosQueAlteramValorParametro/ServicoDePrecificacao.java
@@ -0,0 +1,17 @@
+package revisao.Poo.MetodosQueAlteramValorParametro;
+
+public class ServicoDePrecificacao {
+
+    //observe que a variável alterada morre no escopo do método
+    //ela nao modifica nada.
+    void calcularPrecoVenda(double precoCusto) {
+        //adiciona 20% de margem de lucro
+        precoCusto = precoCusto*1.20;
+    }
+
+    //forma correta
+/*
+    double calcularPrecoVenda(double precoCusto) {
+        return precoCusto*1.20;
+    }*/
+}

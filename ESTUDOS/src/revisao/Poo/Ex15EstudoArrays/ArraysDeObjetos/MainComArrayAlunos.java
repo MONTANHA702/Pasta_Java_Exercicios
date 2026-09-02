19ec13dbd9071fb2237c13da4f41f7bd32bade57
MONTANHA702 <jaderssantos@gmail.com>
2026-09-02 17:49:16 -0300

    iterando arrays de objetos

--- ESTUDOS/src/revisao/Poo/Ex15EstudoArrays/ArraysDeObjetos/MainComArrayAlunos.java
+++ ESTUDOS/src/revisao/Poo/Ex15EstudoArrays/ArraysDeObjetos/MainComArrayAlunos.java
@@ -1,12 +1,14 @@
 package revisao.Poo.Ex15EstudoArrays.ArraysDeObjetos;
 
+import java.util.Arrays;
+
 public class MainComArrayAlunos {
     public static void main(String[] args) {
 
         TurmaComArrayDeAluno turmaB = new TurmaComArrayDeAluno();
         turmaB.identificador = "Turma B";
         turmaB.professor = "Professora Lulu";
-        turmaB.alunos = new Aluno[4]; //instancio um vetor com 4 vetores
+        turmaB.alunos = new Aluno[5]; //instancio um vetor com 4 vetores
 
         turmaB.alunos[0] = new Aluno();//instancio o vetor 0
         turmaB.alunos[0].nome = "Jaja";
@@ -28,5 +30,21 @@ public class MainComArrayAlunos {
         aluno4.idade = 12;
         turmaB.alunos[3] = aluno4;
 
+        //retorna exceção caso algum vetor seja null - usar if-else
+        for (int i = 0; i < turmaB.alunos.length; i++) {
+
+            Aluno aluno = turmaB.alunos[i];
+
+            if (aluno != null) {
+                System.out.printf("%d - %S (%d anos)%n", (i + 1), aluno.nome, aluno.idade);
+            } else {
+                System.out.printf("%d - vago%n", (i + 1));
+            }
+
+        }
+
+        //trazendo do metodo
+        //turmaB.imprimirListaAlunos();
     }
 }
+
--- ESTUDOS/src/revisao/Poo/Ex15EstudoArrays/ArraysDeObjetos/TurmaComArrayDeAluno.java
+++ ESTUDOS/src/revisao/Poo/Ex15EstudoArrays/ArraysDeObjetos/TurmaComArrayDeAluno.java
@@ -5,4 +5,14 @@ public class TurmaComArrayDeAluno {
     String identificador;
     String professor;
     Aluno[] alunos;
+
+    void imprimirListaAlunos() {
+        for (Aluno aluno : alunos) {
+            if (aluno != null) {
+                System.out.printf("%S (%d anos)%n", aluno.nome, aluno.idade);
+            } else {
+                System.out.println("VAGO");
+            }
+        }
+    }
 }

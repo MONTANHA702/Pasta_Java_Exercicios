f3e29e6d94a7e3d5af11586421128ef8c9ed0876
MONTANHA702 <jaderssantos@gmail.com>
2026-09-02 15:52:18 -0300

    representando arrays com o Arrays.toString

---
+++ ESTUDOS/src/revisao/Poo/Ex15EstudoArrays/RepresentandoArraysString.java
@@ -0,0 +1,21 @@
+package revisao.Poo.Ex15EstudoArrays;
+
+import java.util.Arrays;
+
+public class RepresentandoArraysString {
+
+    public static void main(String[] args) {
+
+        int[] vetor = {10, 20, 30, 40, 50};
+/*
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
+*/
+        //usando classe Arrays
+        System.out.println(Arrays.toString(vetor));
+
+
+    }
+}

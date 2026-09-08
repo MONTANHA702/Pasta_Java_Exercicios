a10b5249301fa36cc508f46197eca95cdf109217
MONTANHA702 <jaderssantos@gmail.com>
2026-09-08 18:20:18 -0300

    encadeamento de chamadas de construtores

--- ESTUDOS/src/revisao/Poo/Ex20CriandoConstrutores/Produto.java
+++ ESTUDOS/src/revisao/Poo/Ex20CriandoConstrutores/Produto.java
@@ -11,7 +11,7 @@ public class Produto {
 
     //é uma boa prática validar os construtores, da mesma forma que validamos o métodos
 
-    Produto() {
+    /*Produto() {
 
     }
     Produto(String nome) {
@@ -27,4 +27,26 @@ public class Produto {
         this.nome = nome;
         this.quantidadeEmEstoque = quantidadeEmEstoque;
     }
+*/
+
+    //encadeamento de chamadas de construtores
+
+    Produto() {
+        this("Sem nome");//chama o construtor de baixo, só que no nome fica sem nome.
+
+    }
+    Produto(String nome) {
+        this(nome, QUANTIDADE_EM_ESTOQUE_INICIAL);//pega o construtor de baixo, mas coloca
+                                                    //a quantidade padrã0 (100)
+    }
+    Produto(String nome, int quantidadeEmEstoque) {
+        Objects.requireNonNull(nome, "Nome é obrigatório.");
+        if (quantidadeEmEstoque < 0) {
+            throw new IllegalArgumentException("O estoque não pode ser negativo.");
+        }
+        this.nome = nome;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+
 }
